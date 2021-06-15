cc7e36d7a448fbe21b4ee14e55791817d4a9735c
Vincent <5020559@gmail.com>
2021-06-16 02:01:01 +0800

    upd

--- clock.py
+++ clock.py
@@ -5,16 +5,20 @@ from main import alert_all
 
 sched = BlockingScheduler()
 
-# @sched.scheduled_job('interval', seconds=3)
-# def timed_job():
-#     print('This job is run every three minutes.')
-
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
-def scheduled_job():
+@sched.scheduled_job('interval', seconds=15)
+def timed_job():
     try:
         db = json.load(open('DataBase/DataBase.json', encoding='utf-8'))
     except:
         db = json.loads("{}")
     alert_all(db)
+
+# @sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
+# def scheduled_job():
+#     try:
+#         db = json.load(open('DataBase/DataBase.json', encoding='utf-8'))
+#     except:
+#         db = json.loads("{}")
+#     alert_all(db)
 
 sched.start()
