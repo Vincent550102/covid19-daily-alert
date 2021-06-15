9dd9830835b9ec8aa3c5a00e9c851772140705f9
Vincent <5020559@gmail.com>
2021-06-16 01:58:20 +0800

    test

--- clock.py
+++ clock.py
@@ -1,4 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import json
+from main import alert_all
+
 
 sched = BlockingScheduler()
 
@@ -6,8 +9,12 @@ sched = BlockingScheduler()
 # def timed_job():
 #     print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=2)
 def scheduled_job():
-    print('awake')
+    try:
+        db = json.load(open('DataBase/DataBase.json', encoding='utf-8'))
+    except:
+        db = json.loads("{}")
+    alert_all(db)
 
 sched.start()
