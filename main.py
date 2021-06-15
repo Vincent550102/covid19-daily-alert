e9599c376d913630b28f84054dc6fb4deb60ab7c
Vincent <5020559@gmail.com>
2021-06-15 21:57:14 +0800

    init

---
+++ main.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import (
+    MessageEvent,
+    TextMessage,
+    TextSendMessage,
+    ImageSendMessage,
+    ButtonsTemplate,
+    MessageTemplateAction,
+    TemplateSendMessage
+)
+from random import randint
+from time import sleep
+import configparser,requests,json
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
+handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+
+def push_mess(uid, mess):
+    line_bot_api.push_message(
+        uid,
+        TextMessage(text=mess)
+    )
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+# 學你說話
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    pass
+
+
+if __name__ == "__main__":
+    app.run()
